6da36fb6a720f2c1b8dcb54d1e97de36ff9e6e72
OlegM <x2007@rambler.ru>
2025-01-17 15:26:18 +0500

    Exe

--- BotWeather.py
+++ BotWeather.py
@@ -3,12 +3,15 @@ import requests
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
+from datetime import datetime
 
 from config import TOKEN, ACCUWEATHER_API_KEY
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+# Храним время последнего сообщения
+last_message_time = None
 
 # Функция для получения прогноза погоды
 async def get_weather(city="Chelyabinsk"):
@@ -32,27 +35,30 @@ async def get_weather(city="Chelyabinsk"):
 
     return "Не удалось получить данные о погоде."
 
-
 @dp.message(Command('help'))
 async def help_command(message: Message):
     await message.answer("Этот бот умеет выполнять команды:\n/start - Запуск бота\n/help - Помощь\n/weather - Узнать погоду в Челябинске")
-
 
 @dp.message(CommandStart())
 async def start_command(message: Message):
     await message.answer(
         "Привет! Я бот, который показывает погоду!\nИспользуйте команду /weather для получения прогноза погоды.")
 
-
 @dp.message(Command('weather'))
 async def weather_command(message: Message):
+    global last_message_time
+    current_time = datetime.now()
+
+    if last_message_time is not None:
+        time_diff = current_time - last_message_time
+        await message.answer(f"Прошло времени с последнего сообщения: {round(time_diff.total_seconds()/60)} минут(-а/ы).")
+
+    last_message_time = current_time
     weather_info = await get_weather()
     await message.answer(weather_info)
 
-
 async def main():
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
